f8e08fdab1398b825bc13d6e17d2beb6d00d9604
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-04-02 18:13:28 +0200

    Section02 Boxing

---
+++ Code/Section02/part1/Boxing_and_Unboxing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxing_and_Unboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Boxing
+            //boxing is the process of converting a value type to an object type. to box a value type, you simply assign it to an object variable.
+            // Boxing
+            int valType = 10;
+            object objType = valType; // Boxing
+
+
+            Console.WriteLine("Value Type: " + valType);
+            Console.WriteLine("Object Type: " + objType);
+            #endregion
+
+
+        }
+    }
+}
