8d19d6f6e64c4919e5acbe135fe6b65b3f607090
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-04-03 20:41:55 +0200

    intro to Lists

---
+++ Code/Section03/Collections_Lists/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Lists
+            List<int> numbers = new List<int>();
+
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+            numbers.Add(4);
+            numbers.Add(5);
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+            Console.WriteLine(numbers[3]);
+            Console.WriteLine(numbers[4]);
+
+            Console.WriteLine("\nChange index 2 with value 200");
+            numbers[2] = 200;
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+            Console.WriteLine(numbers[3]);
+            Console.WriteLine(numbers[4]);
+            #endregion
+
+
+        }
+    }
+}
