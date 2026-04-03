8e0c9ae53a6e3916f0a4acbc858dd5849d1a5d9c
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-04-03 20:51:54 +0200

    Section03 -> insert elements into a list

--- Code/Section03/Collections_Lists/Program.cs
+++ Code/Section03/Collections_Lists/Program.cs
@@ -10,6 +10,7 @@ namespace Collections_Lists
     {
         static void Main(string[] args)
         {
+/*
             #region Lists
             List<int> numbers = new List<int>();
 
@@ -33,8 +34,23 @@ namespace Collections_Lists
             Console.WriteLine(numbers[3]);
             Console.WriteLine(numbers[4]);
             #endregion
+*/
 
+            #region insert element into List
 
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //insert at end
+            numbers.Add(1);
+            Console.WriteLine("after insert '1' at end: " + string.Join(", ", numbers));
+
+            //insert at spacific index
+            numbers.Insert(0, 0);//insert 2 at index 0
+            Console.WriteLine("after insert 0 at index 0: " + string.Join(", ", numbers));
+
+            //insert multiple elements 
+            numbers.InsertRange(3, new List<int> { 3, 4, 5 });//insert 3,4,5 at index 3
+            Console.WriteLine("after insert 3,4,5 at index 3: " + string.Join(", ", numbers));
+            #endregion
         }
     }
 }
