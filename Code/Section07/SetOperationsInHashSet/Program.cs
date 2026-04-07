46764ec81bf49208a77f047c701161a7b98b846e
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-04-07 23:14:26 +0200

    Section07 -> Union Operation with HashSet in C#

---
+++ Code/Section07/SetOperationsInHashSet/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetOperationsInHashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Union Operation with HashSet in C#
+
+            HashSet<int> set1 = new HashSet<int> { 1, 2, 3 };
+            HashSet<int> set2 = new HashSet<int> { 3, 4, 5 };
+
+
+            // Union of set1 and set2
+            set1.UnionWith(set2);
+
+
+            Console.WriteLine("Union of sets:");
+            foreach (int item in set1)
+            {
+                Console.WriteLine(item);
+            }
+            #endregion
+        }
+    }
+}
