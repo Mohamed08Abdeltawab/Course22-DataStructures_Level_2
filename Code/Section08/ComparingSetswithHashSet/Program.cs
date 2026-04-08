fffaa17d090daf1fdfd8b08b3ad362f8faa6336d
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-04-08 19:09:57 +0200

    Section08 -> Using Overlaps with HashSet in C#

--- Code/Section08/ComparingSetswithHashSet/Program.cs
+++ Code/Section08/ComparingSetswithHashSet/Program.cs
@@ -37,6 +37,17 @@ namespace ComparingSetswithHashSet
             //set1 is mother of set2
             Console.WriteLine("\nset1 is a superset of set2: " + set1.IsSupersetOf(set2));
             #endregion
+
+            #region Using Overlaps with HashSet in C#
+            set1 = new HashSet<int> { 1, 2, 3 };
+            set2 = new HashSet<int> { 3, 4, 5 };
+            set3 = new HashSet<int> { 6, 7, 8 };
+
+
+            Console.WriteLine("\nset1 overlaps set2: " + set1.Overlaps(set2));
+            Console.WriteLine("set1 overlaps set3: " + set1.Overlaps(set3));
+            Console.ReadKey();
+            #endregion
         }
     }
 }
