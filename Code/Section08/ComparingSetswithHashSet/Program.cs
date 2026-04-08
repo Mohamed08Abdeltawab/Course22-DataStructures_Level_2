3b90018e98e01a5e25afc40d09c0ab03017fe942
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-04-08 18:52:00 +0200

    Section08 -> Using IsSubsetOf with HashSet in C#

--- Code/Section08/ComparingSetswithHashSet/Program.cs
+++ Code/Section08/ComparingSetswithHashSet/Program.cs
@@ -19,6 +19,15 @@ namespace ComparingSetswithHashSet
             Console.WriteLine("set1 equals set2: " + set1.SetEquals(set2)); // True
             Console.WriteLine("set1 equals set3: " + set1.SetEquals(set3)); // False
             #endregion
+
+            #region Using IsSubsetOf with HashSet in C#
+            set1 = new HashSet<int>() { 1, 2, 3 };
+            set2 = new HashSet<int>() { 1, 2, 3, 4, 5, 6};
+
+
+            Console.WriteLine("\nset1 is subset of set2: " + set1.IsSubsetOf(set2)); // True
+            Console.WriteLine("set1 is subset of set3: " + set1.IsSubsetOf(set3));// False
+            #endregion
         }
     }
 }
