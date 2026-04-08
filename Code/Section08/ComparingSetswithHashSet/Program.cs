642e26b794cc2bc234dd1613843eefd929862bad
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-04-08 18:49:20 +0200

    Section08 -> Using SetEquals with HashSet in C#

---
+++ Code/Section08/ComparingSetswithHashSet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparingSetswithHashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Using SetEquals with HashSet in C#
+
+            HashSet<int> set1 = new HashSet<int>() { 1, 2, 3};
+            HashSet<int> set2 = new HashSet<int>() { 1, 2, 3};
+            HashSet<int> set3 = new HashSet<int>() { 3, 4, 5};
+
+            Console.WriteLine("set1 equals set2: " + set1.SetEquals(set2)); // True
+            Console.WriteLine("set1 equals set3: " + set1.SetEquals(set3)); // False
+            #endregion
+        }
+    }
+}
