4d059608e23583a2d28da7bd5e0fb4876d38e274
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-04-10 18:30:20 +0200

    Section10 -> Opeation Example -> Union, Intersection, Differnce, Subset, SuperSet

--- Code/Section10/SortedSet/Program.cs
+++ Code/Section10/SortedSet/Program.cs
@@ -104,7 +104,57 @@ namespace SortedSet
             // Check if set1 is a superset of set2
             bool isSuperset = set1.IsSupersetOf(set2);
             Console.WriteLine("Is set1 a superset of set2? " + isSuperset);
-            Console.ReadKey();
+
+            #endregion
+
+
+            #region Opeation Example -> Union, Intersection, Differnce, Subset, SuperSet
+            set1 = new SortedSet<int>() { 1, 2, 3, 4, 5 };
+            set2 = new SortedSet<int>() { 3, 4, 5, 6, 7 };
+
+            // Union
+            SortedSet<int> unionSet = new SortedSet<int>(set1);
+            unionSet.UnionWith(set2);
+            Console.WriteLine("\nUnion:");
+            foreach (int item in unionSet)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            // Intersection
+            SortedSet<int> intersectSet = new SortedSet<int>(set1);
+            intersectSet.IntersectWith(set2);
+            Console.WriteLine("\nIntersection:");
+            foreach (int item in intersectSet)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            // Difference
+            SortedSet<int> differenceSet = new SortedSet<int>(set1);
+            differenceSet.ExceptWith(set2);
+            Console.WriteLine("\nDifference (set1 - set2):");
+            foreach (int item in differenceSet)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            // Subset and Superset
+            Console.WriteLine("\nSubset:");
+            if (set1.IsSubsetOf(set2))
+                Console.WriteLine("Set1 is a subset of Set2");
+            else
+                Console.WriteLine("Set1 is not a subset of Set2");
+
+            Console.WriteLine("\nSuperset:");
+            if (set1.IsSupersetOf(set2))
+                Console.WriteLine("Set1 is a superset of Set2");
+            else
+                Console.WriteLine("Set1 is not a superset of Set2");
+
 
             #endregion
         }
