6f5e08025f8a0d497d0db64aa51ce8b4a8809b1f
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-04-16 21:43:35 +0200

    Section11 -> Filtering ArrayList with Linq

--- Code/Section12/ArrayList/ArrayList/Program.cs
+++ Code/Section12/ArrayList/ArrayList/Program.cs
@@ -41,3 +41,20 @@ for (int i = 0; i < list2.Count; i++)
 #endregion
 
 
+#region Filtering ArrayList with Linq
+ArrayList arrayList = new ArrayList { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+//we use cast here to convert it to int first then we apply the filter.
+var evenNumbers = arrayList.Cast<int>().Where(num => num % 2 == 0);
+
+
+Console.WriteLine("All even numbers:");
+foreach (var num in evenNumbers)
+{
+    Console.WriteLine(num);
+}
+
+#endregion
+
+
