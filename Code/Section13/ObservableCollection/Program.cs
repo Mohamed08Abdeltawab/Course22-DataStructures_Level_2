55946b057ea3b6d55a56d86974221a50c3e11e63
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-04-16 22:41:19 +0200

    Section13 -> Creating and Adding Items to ObservableCollection

---
+++ Code/Section13/ObservableCollection/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+
+#region Creating and Adding Items to ObservableCollection
+ObservableCollection<string> collection = new ObservableCollection<string>();
+//adding 
+collection.Add("One");
+collection.Add("Two");
+collection.Add("Three");
+
+//print
+foreach(var item in collection)
+{
+    Console.WriteLine(item);
+}
+
+#endregion
