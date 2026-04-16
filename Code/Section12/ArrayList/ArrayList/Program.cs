362c92729436d134281e027ec67c7e5e956206aa
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-04-16 17:41:07 +0200

    Section11 -> ArrayList Working with ArrayList

---
+++ Code/Section12/ArrayList/ArrayList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+#region Working with ArrayList
+ArrayList list = new ArrayList(); // Creating an ArrayList
+list.Add(10); // Adding elements
+list.Add("Hello");
+list.Add(true);
+
+
+Console.WriteLine("Total elements in ArrayList: " + list.Count);
+
+
+Console.WriteLine("Content of ArrayList using index:");
+for (int i = 0; i < list.Count; i++)
+{
+    Console.WriteLine("Index " + i + ": " + list[i]);
+}
+#endregion
