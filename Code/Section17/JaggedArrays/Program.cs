6850f9594b0269e12e0a475d19127884784aef70
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-04-27 22:12:20 +0300

    Section17 -> into to Jagged arrays

---
+++ Code/Section17/JaggedArrays/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Jagged array declaration
+            int[][] jaggedArr1 =
+            {
+                new int[] {1,2,3,4,5,6,7,},
+                new int[] {8,9,10,11,12},
+                new int[] {13,14,15,16,17,18,19,20}
+            };
+
+            int[][] jaggedArr2 = new int[3][];
+            jaggedArr2[0] = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            jaggedArr2[1] = new int[] { 8, 9, 10, 11, 12 };
+            jaggedArr2[2] = new int[] { 13, 14, 15, 16, 17, 18, 19, 20 };
+
+            foreach (int[] arr in jaggedArr1)
+            {
+                foreach(int num in arr)
+                {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+            }
+
+            //or
+            Console.WriteLine();
+
+            for(int i = 0; i<jaggedArr2.Length; i++)
+            {
+                for(int j = 0; j< jaggedArr2[i].Length; j++)
+                {
+                    Console.Write(jaggedArr2[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
