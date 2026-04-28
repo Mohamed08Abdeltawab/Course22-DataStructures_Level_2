660feb3ca7635369e18fd37f99e96a2556ecdc43
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-04-28 16:56:16 +0300

    Section18 -> uisng linq with tuples

--- Code/Section18/Tuples/Program.cs
+++ Code/Section18/Tuples/Program.cs
@@ -20,11 +20,34 @@ namespace Tuples
             var person2 = GetPerson();
             Console.WriteLine("Person2 item1: " + person2.Item1);
             Console.WriteLine("Person2 item2: " + person2.Item2);
-        }
 
+            
+
+            #region using linq in tuple
+
+            List<(int, string, int)> LPerson = new List<(int, string, int)>
+            {
+            (1,"mohamed",22),
+            (2,"ali",33),
+            (3,"karem",44)
+            };
+
+            //filter age less than 30 and print name
+            var PersonLess30 = LPerson.Where(p => p.Item3 < 30).Select(n => n.Item2).ToList();
+            Console.WriteLine("person less than 30:");
+            foreach(var p in PersonLess30)
+            {
+                Console.WriteLine(p);//p hold name
+            }
+
+            var AverageAge = LPerson.Average(p => p.Item3);
+            Console.WriteLine("Average age: " + AverageAge);
+            #endregion
+        }
         static (int, string) GetPerson()
         {
             return (1, "Mohamed");
         }
+
     }
 }
