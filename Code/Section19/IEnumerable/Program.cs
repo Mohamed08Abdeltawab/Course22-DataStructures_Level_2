24635e317be7ef81ac36deef7cf93d4ab370a9db
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-04-28 22:31:47 +0300

    Secion19 -> IEnumerable

---
+++ Code/Section19/IEnumerable/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IEnumerable1
+{
+    public class CustomCollection<T> : IEnumerable<T>
+    {
+        private List<T> _items = new List<T>();
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for(int i = 0; i < _items.Count; i++)
+            {
+                yield return _items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            _items.Add(item);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCollection = new CustomCollection<string>();
+            myCollection.Add("Hello");
+            myCollection.Add("World");
+
+            foreach (var item in myCollection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
