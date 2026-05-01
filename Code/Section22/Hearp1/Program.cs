2602eca4698dc77195a8baabfc0b0da59549d941
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-05-01 17:31:30 +0300

    Section22 -> Min-Heap: Extract (Delete) and HeapifyDown Operations

--- Code/Section22/Hearp1/Program.cs
+++ Code/Section22/Hearp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -51,6 +52,52 @@ namespace Hearp1
 
             return heap[0]; // The smallest element is at the root
         }
+
+        public int ExtractMin()
+        {
+            if(heap.Count == 0)
+            {
+                throw new InvalidOperationException("Heap is empty.");
+            }
+
+            //we need to remove the min element and return it
+            int min = heap[0];//return it
+
+            heap[0] = heap[heap.Count - 1];//move the last element to the root
+
+            heap.RemoveAt(heap.Count - 1);//remove the last element to resize the heap
+
+            HeapyifyDown(0);//heapify down from the root to restore the heap property
+            return min;
+        }
+
+        public void HeapyifyDown(int index)
+        {
+            while (index < heap.Count)
+            {
+                int smallest = index;
+
+                int leftChild = 2 * index + 1;
+                int rightChild = 2 * index + 2;
+
+                if (leftChild < heap.Count && heap[leftChild] < heap[smallest])
+                {
+                    smallest = leftChild;
+                }
+
+                if (rightChild < heap.Count && heap[rightChild] < heap[smallest])
+                {
+                    smallest = rightChild;
+                }
+
+                //if not access to two if statements, we can break the loop if the smallest is still the current index, which means we are done
+                if (smallest == index) break;//if the smallest is still the current index, we are done 
+
+                (heap[index], heap[smallest]) = (heap[smallest], heap[index]);
+
+                index = smallest;
+            }
+        }
     }
     internal class Program
     {
@@ -69,6 +116,14 @@ namespace Hearp1
             minHeap.DisplayHeap();
 
             Console.WriteLine(minHeap.Peek());
+
+            // Extract elements based on priority
+            Console.WriteLine("\nExtracting elements from the Min-Heap:");
+            Console.WriteLine("Extracted Minimum: " + minHeap.ExtractMin());
+            minHeap.DisplayHeap();
+
+            Console.WriteLine("\nExtracted Minimum: " + minHeap.ExtractMin());
+            minHeap.DisplayHeap();
         }
     }
 }
